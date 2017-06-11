d57052f242d8713e748125aad449e01c49e8fba3
LINMouMouZiBo <673418747@qq.com>
2017-06-11 20:21:22 +0800

    remove useless api

--- src/com/sysu/controller/YueyinController.java
+++ src/com/sysu/controller/YueyinController.java
@@ -13,7 +13,6 @@ import javax.servlet.http.HttpSession;
 
 import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
-import org.apache.poi.ss.formula.ptg.StringPtg;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
